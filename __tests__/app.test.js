51150bc7362f1ac3eb8124ed271b64b21f0df23e
Dan Minkevitch <dan@alchemycodelab.com>
2022-01-20 12:15:24 -0800

    add initial test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,16 @@ describe('backend-refactory routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should be able to list orders', async () => {
+    const res = await request(app).get('/api/v1/orders');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        quantity: 1,
+        product: 'Widget',
+      },
+    ]);
+  });
 });
