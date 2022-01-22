ddb0f8b81641252f4aa4b612df6409d951d27076
Dan Minkevitch <dan@alchemycodelab.com>
2022-01-22 15:51:10 -0800

    complete order functionality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 describe('refactory routes', () => {
   beforeEach(() => {
@@ -12,15 +13,59 @@ describe('refactory routes', () => {
     pool.end();
   });
 
+  it('should be able to create an order', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ product: 'Widget', quantity: 1 });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      product: 'Widget',
+      quantity: 1,
+    });
+  });
+
+  it('should be able to list an order by id', async () => {
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
+    const res = await request(app).get(`/api/v1/orders/${order.id}`);
+
+    expect(res.body).toEqual(order);
+  });
+
   it('should be able to list orders', async () => {
+    await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get('/api/v1/orders');
 
     expect(res.body).toEqual([
       {
         id: expect.any(String),
-        quantity: 1,
         product: 'Widget',
+        quantity: 1,
       },
     ]);
+  });
+
+  it('should be able to update an order', async () => {
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
+    const res = await request(app)
+      .patch(`/api/v1/orders/${order.id}`)
+      .send({ product: 'Thingamajig', quantity: 2 });
+
+    const expected = {
+      id: expect.any(String),
+      product: 'Thingamajig',
+      quantity: 2,
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await Order.getById(order.id)).toEqual(expected);
+  });
+
+  it('should be able to delete an order', async () => {
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
+    const res = await request(app).delete(`/api/v1/orders/${order.id}`);
+
+    expect(res.body).toEqual(order);
+    expect(await Order.getById(order.id)).toBeNull();
   });
 });
--- data/setup.js
+++ data/setup.js
@@ -3,5 +3,9 @@ const fs = require('fs').promises;
 module.exports = (pool) => {
   return fs
     .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
-    .then((sql) => pool.query(sql));
+    .then((sql) => pool.query(sql))
+    .then(() => console.log('✅ Database setup complete!'))
+    .catch((error) =>
+      console.error('❌  Error setting up database:', error.message)
+    );
 };
