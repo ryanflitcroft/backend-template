a86f20f62273728407728d5553d9b1bf3ae9f844
Dan Minkevitch <dan@alchemycodelab.com>
2022-01-22 15:18:27 -0800

    initial commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-refactory routes', () => {
+describe('refactory routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 
+const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+  console.log(`🚀  Server started on ${API_URL}:${PORT}`);
 });
 
 process.on('exit', () => {
-  console.log('Goodbye!');
+  console.log('👋  Goodbye!');
   pool.end();
 });
