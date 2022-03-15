7130e7247a1f9b33a41825e8af95b3083e58822d
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-14 18:46:35 -0700

    Refactor test for insert.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,13 +5,13 @@ const app = require('../lib/app');
 const Order = require('../lib/models/Order');
 
 // TODO: Remove this function & use the Order model
-async function createOrder({ product, quantity }) {
-  const { rows } = await pool.query(
-    'INSERT INTO orders(product, quantity) VALUES ($1, $2) RETURNING *;',
-    [product, quantity]
-  );
-  return new Order(rows[0]);
-}
+// async function createOrder({ product, quantity }) {
+//   const { rows } = await pool.query(
+//     'INSERT INTO orders(product, quantity) VALUES ($1, $2) RETURNING *;',
+//     [product, quantity]
+//   );
+//   return new Order(rows[0]);
+// }
 
 // TODO: Remove this function & use the Order model
 // async function getOrderById(id) {
@@ -47,14 +47,14 @@ describe('refactory routes', () => {
   });
 
   it('should be able to list an order by id', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
   });
 
   it('should be able to list orders', async () => {
-    await createOrder({ product: 'Widget', quantity: 1 });
+    await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get('/api/v1/orders');
 
     expect(res.body).toEqual([
@@ -67,7 +67,7 @@ describe('refactory routes', () => {
   });
 
   it('should be able to update an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app)
       .patch(`/api/v1/orders/${order.id}`)
       .send({ product: 'Thingamajig', quantity: 2 });
@@ -83,7 +83,7 @@ describe('refactory routes', () => {
   });
 
   it('should be able to delete an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).delete(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
